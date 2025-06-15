797a96ecf6708b2828e483b5cd41d606d723d541
WaruCodes <sdrwarushika@std.appsc.sab.ac.lk>
2025-06-15 14:49:20 +0530

    Add Tailwind and PostCSS config

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'], // Adjust paths as needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
